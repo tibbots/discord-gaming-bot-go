f665e727803287cc2229682978796818e82b2008
codejanovic <codejanovic@gmail.com>
2018-12-02 22:42:12 +0100

    implemented help command

--- handler/help.go
+++ handler/help.go
@@ -1,23 +1,55 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/bwmarrin/discordgo"
-	"github.com/tibbots/discord-gaming-bot-go/logging"
 )
 
 type helpCommandHandler struct {
-}
-
-var helpCommandHandlerInstance *helpCommandHandler
-
-func init() {
-	helpCommandHandlerInstance = &helpCommandHandler{}
+	helpMessages *discordgo.MessageEmbed
 }
 
 func (h *helpCommandHandler) Handle(s *discordgo.Session, m *discordgo.MessageCreate) {
-	logging.Info().Msg("some message has been created")
+	event := GetMessageCreatedEvent(s, m)
+	if !event.shouldBeHandled() || !event.isTalkingToMe() || (!event.isCommand("help") && !event.isCommand("rtfm")) {
+		return
+	}
+
+	if h.helpMessages == nil {
+		h.helpMessages = &discordgo.MessageEmbed{
+			Title: "Discord Gaming Bot Manual",
+			Footer: &discordgo.MessageEmbedFooter{
+				Text: "made by tibbot.org",
+			},
+
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:  "Creating a Profile",
+					Value: fmt.Sprintf("@%s create profile (or via direct message)", s.State.User.Username),
+				},
+				{
+					Name:  "Deleting a Profile",
+					Value: fmt.Sprintf("@%s delete profile (or via direct message)", s.State.User.Username),
+				},
+				{
+					Name:  "Inspecting your Profile",
+					Value: fmt.Sprintf("@%s show profile (or via direct message)", s.State.User.Username),
+				},
+				{
+					Name:  "Inspecting another persons Profile",
+					Value: fmt.Sprintf("@%s show profile @YourFriend (or via direct message)", s.State.User.Username),
+				}, {
+					Name:  "Adding an platform account (like Steam, Origin or whatsoever)",
+					Value: fmt.Sprintf("@%s add account [platform] [account-id] (or via direct message)", s.State.User.Username),
+				},
+			},
+		}
+	}
+
+	_, _ = s.ChannelMessageSendEmbed(m.ChannelID, h.helpMessages)
+
 }
 
 func GetHelpCommandHandler() MessageCreatedHandler {
-	return helpCommandHandlerInstance
+	return &helpCommandHandler{}
 }
